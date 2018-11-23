fe3412392cc566a45e2713b6143d9ebc6964809f
Anthony Mochel <tony.mochel@gmail.com>
2018-11-23 21:06:31 +0100

    feat(handler): add color handler

---
+++ Monopoly/Monopoly/Handlers/ColorHandler.cs
@@ -0,0 +1,32 @@
+﻿using Monopoly.Resources.Colors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Handlers
+{
+    public class ColorHandler
+    {
+        #region Variables
+        private static ColorHandler _instance = null;
+        private static List<string> _pawnColors;
+        private static int _colorIndex;
+        #endregion
+
+        #region Constructeurs
+        /// <summary>
+        /// Création d'une instance de la classe
+        /// </summary>
+        public ColorHandler()
+        {
+            _pawnColors = PawnColors.Colors;
+            _colorIndex = 0;
+        }
+
+        
+        #endregion
+        
+    }
+}
