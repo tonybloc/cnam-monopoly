3a5ce20b5a095c4616ecdbccc34c2725423d1730
Anthony Mochel <tony.mochel@gmail.com>
2018-11-23 21:26:11 +0100

    feat(manager): add game manager with singleton

---
+++ Monopoly/Monopoly/Handlers/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Handlers
+{
+    public class GameManager
+    {
+        #region Variables
+        private static GameManager instance = null;
+        public PlayerHandler playerHandler { get; private set; }
+        #endregion
+
+        #region Constructeurs
+        /// <summary>
+        /// Crée une instance de la classe
+        /// </summary>
+        private GameManager()
+        {
+            this.playerHandler = PlayerHandler.Instance;
+        }
+
+        /// <summary>
+        /// Récupère l'instance de la classe
+        /// </summary>
+        public GameManager Instance
+        {
+            get
+            {
+                if(instance == null)
+                {
+                    instance = new GameManager();
+                }
+                return instance;
+            }
+        }
+        #endregion
+    }
+}
