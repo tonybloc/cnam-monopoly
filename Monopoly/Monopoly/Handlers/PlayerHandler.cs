a3238cda7885cfa538c889d9449532157eeec11f
Anthony Mochel <tony.mochel@gmail.com>
2018-11-23 20:38:27 +0100

    feat(handler): add player handler

---
+++ Monopoly/Monopoly/Handlers/PlayerHandler.cs
@@ -0,0 +1,48 @@
+﻿using Monopoly.Models.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly.Handlers
+{
+    public class PlayerHandler
+    {
+        #region Variables
+
+        private List<Player> ListOfPlayers { get; set; }
+
+        #endregion
+
+        #region Constructeurs
+        /// <summary>
+        /// Crée une instance de la classe
+        /// </summary>
+        public PlayerHandler()
+        {
+            this.ListOfPlayers = new List<Player>();
+        }
+        #endregion
+
+        #region Methodes (public)
+        /// <summary>
+        /// Ajoute un joueur dans la liste
+        /// </summary>
+        /// <param name="player"></param>
+        public void AddPlayer(Player player)
+        {
+            this.ListOfPlayers.Add(player);
+        }
+
+        /// <summary>
+        /// Enlève un joueur dans la liste
+        /// </summary>
+        /// <param name="player"></param>
+        public void RemovePlayer(Player player)
+        {
+            this.ListOfPlayers.Remove(player);
+        }
+        #endregion
+    }
+}
