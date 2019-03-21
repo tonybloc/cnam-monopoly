7d40beab98df7c1d0de0e398942deee64977e391
Carole Treser <carole.treser@etu.unistra.fr>
2019-03-21 22:48:24 +0100

    feat(Game_rules): Show game rules

---
+++ Monopoly/Monopoly/View/RulesInterface.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Monopoly.View
+{
+    /// <summary>
+    /// Logique d'interaction pour RulesInterface.xaml
+    /// </summary>
+    public partial class RulesInterface : Page
+    {
+        private const string DisableScriptError =
+    @"function noError() {
+          return true;
+       }       
+
+       window.onerror = noError;";
+
+        public RulesInterface()
+        {
+            try
+            {
+
+                InitializeComponent();
+                clsWebbrowser_Errors.SuppressscriptErrors(webBrowser, true);
+                this.webBrowser.Navigate("http://www.regledujeu.fr/monopoly/");
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show("Une connection Internet est requise.");
+            }
+        }
+      
+        private void ReturnToMenu(object sender, RoutedEventArgs e)
+        {
+            ((MainWindow)Window.GetWindow(this)).MainContent.Content = "";
+            ((MainWindow)Window.GetWindow(this)).MenuContent.Visibility = Visibility.Visible;
+        }
+
+       
+
+    }
+}
+
